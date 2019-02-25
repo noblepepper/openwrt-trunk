df75c60166feb39516d91a0207c9ba8ac0dda226
Noble Pepper <noblepepper@gmail.com>
2019-02-25 04:50:52 -0600

    port dogpatch spi tty driver

--- target/linux/ar71xx/modules.mk
+++ target/linux/ar71xx/modules.mk
@@ -67,3 +67,19 @@ define KernelPackage/spi-vsc7385/description
 endef
 
 $(eval $(call KernelPackage,spi-vsc7385))
+
+define KernelPackage/spi-tty-ds
+  SUBMENU:=$(SPI_MENU)
+  TITLE:=DogStick SPI TTY Driver
+  DEPENDS:=@TARGET_ar71xx
+  KCONFIG:=CONFIG_SPI_TTY_DS
+  FILES:=$(LINUX_DIR)/drivers/spi/spi-tty-ds.ko
+  AUTOLOAD:=$(call AutoLoad,95,spi-tty-ds)
+endef
+
+define KernelPackage/spi-tty-ds/description
+  This package contains the TTY SPI driver.
+endef
+
+$(eval $(call KernelPackage,spi-tty-ds))
+
