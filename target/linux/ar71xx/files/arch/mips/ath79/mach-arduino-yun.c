4daa47e3110a0e9f9c7e911c9ce3ecb39dbbeb9e
Noble Pepper <noblepepper@gmail.com>
2019-03-02 07:42:47 -0600

    interrupt registers in kernel, lots of printks

--- target/linux/ar71xx/files/arch/mips/ath79/mach-arduino-yun.c
+++ target/linux/ar71xx/files/arch/mips/ath79/mach-arduino-yun.c
@@ -19,8 +19,11 @@
 #include "machtypes.h"
 #include <asm/mach-ath79/ar71xx_regs.h>
 #include <asm/mach-ath79/ath79.h>
+#include <asm/mach-ath79/mach-linino.h>
 #include <linux/gpio.h>
 #include "common.h"
+#include <linux/spi/spi_gpio.h>
+#include <linux/platform_device.h>
 
 // Uncomment to have reset on gpio18 instead of gipo7
 #define DS2_B
@@ -71,6 +74,63 @@ static struct gpio_keys_button ds_gpio_keys[] __initdata = {
 	},
 };
 
+
+/* * * * * * * * * * * * * * * * * * * SPI * * * * * * * * * * * * * * * * * */
+
+/*
+ * The SPI bus between the main processor and the MCU is available only in the
+ * following board: YUN, FREEDOG
+ */
+
+static struct spi_gpio_platform_data spi_bus1 = {
+	.sck = LININO_GPIO_SPI_SCK,
+	.mosi = LININO_GPIO_SPI_MOSI,
+	.miso = LININO_GPIO_SPI_MISO,
+	.num_chipselect = LININO_N_SPI_CHIP_SELECT,
+};
+
+static struct platform_device linino_spi1_device = {
+	.name	= "spi_gpio",
+	.id	= 1,
+	.dev.platform_data = &spi_bus1,
+};
+
+/* SPI devices on Linino */
+static struct spi_board_info linino_spi_info[] = {
+	/*{
+		.bus_num		= 1,
+		.chip_select		= 0,
+		.max_speed_hz		= 10000000,
+		.mode			= 0,
+		.modalias		= "spidev",
+		.controller_data	= (void *) SPI_CHIP_SELECT,
+	},*/
+	{
+		.bus_num		= 1,
+		.chip_select		= 0,
+		.max_speed_hz		= 10000000, /* unused but required */
+		.mode			= 0,
+		.modalias		= "atmega32u4",
+		.controller_data	= (void *) LININO_GPIO_SPI_CS0,
+		.platform_data		= (void *) LININO_GPIO_SPI_INTERRUPT,
+	},
+};
+
+/**
+ * Enable the software SPI controller emulated by GPIO signals
+ */
+static void ds_register_spi(void) {
+	pr_info("mach-linino: enabling GPIO SPI Controller");
+
+	/* Enable level shifter on SPI signals */
+	gpio_set_value(DS_GPIO_OE, 1);
+	/* Enable level shifter on AVR interrupt */
+	gpio_set_value(DS_GPIO_OE2, 1);
+	/* Register SPI devices */
+	spi_register_board_info(linino_spi_info, ARRAY_SIZE(linino_spi_info));
+	/* Register GPIO SPI controller */
+	platform_device_register(&linino_spi1_device);
+}
 static void __init ds_common_setup(void)
 {
 	static u8 mac[6];
@@ -152,6 +212,9 @@ static void __init ds_setup(void)
 	if (gpio_request_one(DS_GPIO_OE2,
 	    GPIOF_OUT_INIT_HIGH | GPIOF_EXPORT_DIR_FIXED, "OE-2") != 0)
 		printk("Error setting GPIO OE2\n");
+	
+	/* Register Software SPI controller */
+	ds_register_spi();
 }
 
 MIPS_MACHINE(ATH79_MACH_ARDUINO_YUN, "Yun", "Arduino Yun", ds_setup);
